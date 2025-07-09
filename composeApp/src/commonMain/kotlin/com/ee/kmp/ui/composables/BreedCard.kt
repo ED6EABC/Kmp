572f1bc210c868d8742a6299b384b81f658176c4
EDGAR ADAN ELIZARRARAS VARGAS <edgar.elizarraras@elektra.com.mx>
2025-07-09 16:51:25 -0600

    - Fixed image into BreedCard.kt

--- composeApp/src/commonMain/kotlin/com/ee/kmp/ui/composables/BreedCard.kt
+++ composeApp/src/commonMain/kotlin/com/ee/kmp/ui/composables/BreedCard.kt
@@ -1,25 +1,34 @@
 package com.ee.kmp.ui.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import com.ee.kmp.data.model.Breed
 import com.ee.kmp.ui.colorRed
 import kmp.composeapp.generated.resources.Res
+import kmp.composeapp.generated.resources.error_svgrepo_com
 import kmp.composeapp.generated.resources.heart_straight_fill_svgrepo_com
 import kmp.composeapp.generated.resources.trash_alt_svgrepo_com
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -36,17 +45,40 @@ fun BreedCard(
     onClick: (Breed) -> Unit = {},
     onDelete: (String) -> Unit = {}
 ) {
+
+    val painter = rememberAsyncImagePainter("https://cdn2.thecatapi.com/images/${breed.referenceImageId}.jpg")
+    val state by painter.state.collectAsState()
+
     Card(
         modifier = Modifier.padding(top = 8.dp),
         onClick = { onClick(breed) }
     ) {
         Row(modifier = Modifier.padding(8.dp)) {
-            AsyncImage(
-                model = "https://cdn2.thecatapi.com/images/${breed.referenceImageId}.jpg",
-                contentDescription = breed.name,
-                modifier = Modifier.size(50.dp).weight(1f),
-                contentScale = ContentScale.Crop
-            )
+
+            val imageModifier = Modifier
+                .weight(0.8f)
+                .aspectRatio(16f / 9f)
+
+            when (state) {
+                is AsyncImagePainter.State.Error -> {
+                    Image(
+                        painterResource(Res.drawable.error_svgrepo_com),
+                        contentDescription = "Error loading image",
+                        modifier = imageModifier
+                    )
+                }
+                is AsyncImagePainter.State.Success -> {
+                    Image(
+                        painter = painter,
+                        contentDescription = breed.name,
+                        modifier = imageModifier,
+                        contentScale = ContentScale.Fit
+                    )
+                }
+                else -> {
+                    CircularProgressIndicator( modifier = imageModifier)
+                }
+            }
 
             Spacer(modifier = Modifier.size(8.dp))
 
